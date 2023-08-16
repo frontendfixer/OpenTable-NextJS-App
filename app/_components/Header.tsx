2119077934eba56d8a72683c731ff4270f15d51d
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-16 09:00:50 +0530

    fix: large font size in small screen

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="bg-gradient-to-r from-[#0f1f47] to-[#5f6984] px-4 py-6">
       <div className="mt-10 text-center">
-        <h1 className="mb-2 text-4xl font-bold text-white md:text-5xl">
+        <h1 className="mb-2 text-3xl font-bold text-white md:text-5xl">
           Find your table for any occasion
         </h1>
       </div>
