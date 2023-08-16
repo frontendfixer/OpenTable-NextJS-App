dde5b6751739eade7a2f7bf4ccf6ea24ec873ccc
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-16 09:35:32 +0530

    fixing restaurant card design

--- app/search/_components/RestaurantCard.tsx
+++ app/search/_components/RestaurantCard.tsx
@@ -1,21 +1,19 @@
+import Image from 'next/image'
 import Link from 'next/link'
+import resPic from '@/assets/41710620.webp'
 
 const RestaurantCard = () => {
   return (
-    <div className="flex border-b pb-5">
-      <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-        alt=""
-        className="w-44 rounded"
-      />
+    <div className="mt-5 flex border-b pb-5">
+      <Image src={resPic} alt="" className="w-40 rounded" />
 
-      <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+      <div className="flex flex-col justify-center pl-5">
+        <h2 className="text-3xl font-medium">Aiāna Restaurant Collective</h2>
         <div className="flex items-start">
           <div className="mb-2 flex">*****</div>
           <p className="ml-2 text-sm">Awesome</p>
         </div>
-        <div className="mb-9">
+        <div className="mb-8">
           <div className="flex text-reg font-light">
             <p className="mr-4">$$$</p>
             <p className="mr-4">Mexican</p>
