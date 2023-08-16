974ab93e4a3053f2a62af8a24fac1da121596885
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-16 10:50:33 +0530

    make searchBar a client component

--- app/_components/SearchBar.tsx
+++ app/_components/SearchBar.tsx
@@ -1,7 +1,13 @@
+'use client'
+
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const SearchBar = () => {
+  const router = useRouter()
+  const [location, setLocation] = useState('')
   return (
     <div className=" mx-auto flex flex-col items-center justify-center gap-4 px-10 py-3 text-left md:flex-row">
       <div className="relative">
@@ -13,9 +19,16 @@ const SearchBar = () => {
           type="text"
           className="w-80 rounded border-none p-2 pl-10 text-lg outline-none md:w-96"
           placeholder="City, Town, or Restaurant"
+          onChange={(e) => setLocation(() => e.target.value)}
         />
       </div>
-      <button className="w-80 rounded bg-red-600 px-8 py-2 text-lg font-medium text-white md:w-max">
+      <button
+        onClick={() => {
+          if (location === 'banana') return
+          router.push('/search')
+        }}
+        className="w-80 rounded bg-red-600 px-8 py-2 text-lg font-medium text-white md:w-max"
+      >
         Let&apos;s go
       </button>
     </div>
