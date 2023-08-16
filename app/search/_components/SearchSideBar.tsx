b5b2d77d63d2f51fc9cfbc8cc72a2dbe72926bda
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-16 09:34:57 +0530

    add icon for search category

--- app/search/_components/SearchSideBar.tsx
+++ app/search/_components/SearchSideBar.tsx
@@ -1,8 +1,18 @@
+import {
+  faLocationDot,
+  faMoneyBill1,
+  faUtensils,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const SearchSideBar = () => {
   return (
-    <div className="w-1/5">
+    <>
       <div className="border-b pb-4">
-        <h1 className="mb-2">Region</h1>
+        <h1 className="-ml-5 mb-1 flex items-center gap-1">
+          <FontAwesomeIcon icon={faLocationDot} className="h-4 w-4" />
+          <span className="text-lg">Region</span>
+        </h1>
         <p className="text-reg font-light">Toronto</p>
         <p className="text-reg font-light">Ottawa</p>
         <p className="text-reg font-light">Montreal</p>
@@ -11,13 +21,19 @@ const SearchSideBar = () => {
         <p className="text-reg font-light">Niagara</p>
       </div>
       <div className="mt-3 border-b pb-4">
-        <h1 className="mb-2">Cuisine</h1>
+        <h1 className="-ml-5 mb-1 flex items-center gap-1">
+          <FontAwesomeIcon icon={faUtensils} className="h-4 w-4" />
+          <span className="text-lg">Cuisine</span>
+        </h1>
         <p className="text-reg font-light">Mexican</p>
         <p className="text-reg font-light">Italian</p>
         <p className="text-reg font-light">Chinese</p>
       </div>
       <div className="mt-3 pb-4">
-        <h1 className="mb-2">Price</h1>
+        <h1 className="-ml-5 mb-1 flex items-center gap-1">
+          <FontAwesomeIcon icon={faMoneyBill1} className="h-4 w-4" />
+          <span className="text-lg">Price</span>
+        </h1>
         <div className="flex">
           <button className="w-full rounded-l border p-2 text-reg font-light">
             $
@@ -30,7 +46,7 @@ const SearchSideBar = () => {
           </button>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
