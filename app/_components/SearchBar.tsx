5fc490a944a17abff59fb18c7e4892e9580dd168
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-16 08:59:07 +0530

    fix: search bar not containing in small size

--- app/_components/SearchBar.tsx
+++ app/_components/SearchBar.tsx
@@ -3,19 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SearchBar = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 py-3 text-left md:flex-row">
-      <div className="item-center flex">
+    <div className=" mx-auto flex flex-col items-center justify-center gap-4 px-10 py-3 text-left md:flex-row">
+      <div className="relative">
         <FontAwesomeIcon
           icon={faSearch}
-          className="w-6 rounded-bl rounded-tl bg-white pl-2 text-gray-400"
+          className="absolute left-3 top-3 h-5 w-5 bg-white text-gray-400"
         />
         <input
           type="text"
-          className="rounded-br rounded-tr border-none p-2  text-lg outline-none"
+          className="w-80 rounded border-none p-2 pl-10 text-lg outline-none md:w-96"
           placeholder="City, Town, or Restaurant"
         />
       </div>
-      <button className=" rounded bg-red-600 px-8 py-2 text-lg font-medium text-white">
+      <button className="w-80 rounded bg-red-600 px-8 py-2 text-lg font-medium text-white md:w-max">
         Let&apos;s go
       </button>
     </div>
