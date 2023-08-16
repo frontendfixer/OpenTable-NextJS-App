c8cc2975f44ee2f01225518c50658719812d877d
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-16 17:29:35 +0530

    fix: remove logo text border

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -8,24 +8,24 @@ const Navbar = () => {
     <nav className="flex items-center justify-between bg-white px-4 py-2">
       <Link
         href="/"
-        className="item-center flex gap-1 border-[1px] border-red-500 pr-2 text-xl font-bold text-gray-700 transition-colors duration-200 hover:text-red-700"
+        className="item-center flex gap-1 py-2 pr-2 text-xl font-bold text-gray-700 transition-colors duration-200 hover:text-red-700"
       >
         <Image
           src="/logo.svg"
-          width={24}
-          height={24}
+          width={27}
+          height={27}
           alt=""
           aria-hidden
-          className="bg-red-500 p-1"
+          className="p-1"
         />
         OpenTable
       </Link>
       <div className="flex gap-4">
-        <button className="flex items-center gap-1 rounded-md border bg-blue-500 px-4 py-1 text-white transition hover:opacity-80">
+        <button className="flex items-center gap-1 rounded-md border bg-blue-500 px-4 py-2 text-white transition hover:opacity-80">
           <FontAwesomeIcon icon={faRightToBracket} className="w-4" />
           <span className="hidden font-medium md:block">Sign in</span>
         </button>
-        <button className="flex items-center gap-1 rounded-md border px-4 py-1 transition hover:border-blue-500">
+        <button className="flex items-center gap-1 rounded-md border px-4 py-2 transition hover:border-blue-500">
           <FontAwesomeIcon icon={faUserPlus} className="w-4" />
           <span className="hidden font-medium md:block">Sign up</span>
         </button>
