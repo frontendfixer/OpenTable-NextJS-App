9dbb8fae952e6040740189284bd3238548843918
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-18 20:54:16 +0530

    search page loading spinner

---
+++ app/search/loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="grid h-80 w-full place-content-center">
+      <div class="h-20 w-20 animate-spin rounded-full border-8 border-t-blue-400"></div>
+    </div>
+  )
+}
+
+export default Loading
