fc7d84ffe3667b2d2d7cbd622b1065274c681279
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-18 12:43:56 +0530

    fix: review card responsiveness

--- app/restaurant/[slug]/_components/ReviewCard.tsx
+++ app/restaurant/[slug]/_components/ReviewCard.tsx
@@ -4,25 +4,17 @@ import { Review } from '@prisma/client'
 const ReviewCard = ({ review }: { review: Review }) => {
   const { first_name, last_name, rating, text } = review
   return (
-    <div className="flex">
-      <div className="flex w-1/6 flex-col items-center">
-        <div className="flex h-16 w-16 items-center justify-center rounded-full bg-blue-400">
-          <h2 className="text-2xl text-white">
-            {first_name.charAt(0).toUpperCase() +
-              last_name.charAt(0).toUpperCase()}
-          </h2>
-        </div>
-        <p className="text-center">{first_name + ' ' + last_name}</p>
+    <div className="flex items-center justify-start border p-3">
+      <div className="flex h-16 w-16 items-center justify-center rounded-full bg-blue-400">
+        <h2 className="text-2xl text-white">
+          {first_name.charAt(0).toUpperCase() +
+            last_name.charAt(0).toUpperCase()}
+        </h2>
       </div>
-      <div className="ml-10 w-5/6">
-        <div className="flex items-center">
-          <div className="mb-2 mr-5 flex">
-            {<Starts rating={rating} reviews={[]} />}
-          </div>
-        </div>
-        <div className="mt-5">
-          <p className="text-lg font-light">{text}</p>
-        </div>
+      <div className="ml-6 flex w-5/6 flex-col items-start">
+        <p className="text-xl font-bold">{first_name + ' ' + last_name}</p>
+        <Starts rating={rating} reviews={[]} />
+        <p className="mt-2 font-light text-gray-600">{text}</p>
       </div>
     </div>
   )
