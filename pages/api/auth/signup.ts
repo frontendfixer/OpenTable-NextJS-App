ac3fa3518ca4dde0259027496a10248f85fd28f9
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-19 11:09:34 +0530

    create user api

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,85 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'POST') {
+    const {
+      body: { firstName, lastName, email, phone, city, password },
+    } = req
+
+    const errors: string[] = []
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, { min: 4, max: 20 }),
+        errorMessage: 'First name is invalid',
+      },
+      {
+        valid: validator.isLength(lastName, { min: 4, max: 20 }),
+        errorMessage: 'Last name is invalid',
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: 'Email is invalid',
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: 'Phone number is invalid',
+      },
+      {
+        valid: validator.isLength(city, { min: 4, max: 12 }),
+        errorMessage: 'City is invalid',
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: 'Password is not strong',
+      },
+    ]
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage)
+      }
+    })
+
+    if (errors.length) {
+      return res.status(400).json({ errorMessage: errors[0] })
+    }
+
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: 'Email is associated with another account' })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        email,
+        city,
+        phone,
+      },
+    })
+
+    res.status(200).json({
+      user,
+    })
+  }
+}
