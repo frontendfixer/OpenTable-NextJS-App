37f3d5cd0454fb2493cb9fcc669e7e6fc768693d
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-21 11:09:59 +0530

    fix: no-unescaped entity fixed: using &apos;

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,9 @@ const Error = ({ error }: { error: Error }) => {
       <Image src={errorMascot} width={200} height={400} alt="error" />
       <div className="rounded bg-white px-9 py-14 shadow">
         <h3 className="text-3xl font-bold">Well this is embarrassing</h3>
-        <p className="font bold text-reg">We couldn't found that restaurant </p>
+        <p className="font bold text-reg">
+          We couldn&apos;t found that restaurant{' '}
+        </p>
         <p className="mt-5">Error Code: 400</p>
       </div>
     </div>
