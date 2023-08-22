4c350da806756da4294fa75d5b9f19f7e0172f2e
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-22 19:37:49 +0530

    add signOut button to navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,8 +1,17 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
 import AuthModal from './AuthModal'
+import { useContext } from 'react'
+import { AuthenticationContext } from '../context/AuthContext'
+import useAuth from '@/hooks/useAuth'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+  const { loading, data } = useContext(AuthenticationContext)
+  const { signOut } = useAuth()
   return (
     <nav className="flex items-center justify-between bg-white px-4 py-2">
       <Link
@@ -19,10 +28,26 @@ const Navbar = () => {
         />
         OpenTable
       </Link>
-      <div className="flex gap-4">
-        <AuthModal isSignIn={true} />
-        <AuthModal isSignIn={false} />
-      </div>
+      <>
+        {loading ? null : (
+          <div className="flex gap-4">
+            {data ? (
+              <button
+                className="flex items-center gap-1 rounded-md border bg-red-500 px-4 py-2 text-white transition hover:opacity-80"
+                onClick={signOut}
+              >
+                <FontAwesomeIcon icon={faRightFromBracket} className="w-4" />
+                <span className="hidden font-medium md:block">Sign in</span>
+              </button>
+            ) : (
+              <>
+                <AuthModal isSignIn={true} />
+                <AuthModal isSignIn={false} />
+              </>
+            )}
+          </div>
+        )}
+      </>
     </nav>
   )
 }
