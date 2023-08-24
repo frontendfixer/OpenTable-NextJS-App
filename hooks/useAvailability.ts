9ccc77dab417501204006954bb2d777bdd30452b
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-24 10:46:43 +0530

    create useAvailability hooks

---
+++ hooks/useAvailability.ts
@@ -0,0 +1,45 @@
+'use client'
+
+import axios from 'axios'
+import { useState } from 'react'
+
+const useAvailability = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [data, setData] = useState(null)
+
+  const fetchAvailability = async ({
+    slug,
+    day,
+    time,
+    partySize,
+  }: {
+    slug: string
+    day: string
+    time: string
+    partySize: string
+  }) => {
+    setLoading(true)
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/restaurant/${slug}/availability`,
+        {
+          params: {
+            day,
+            time,
+            partySize,
+          },
+        },
+      )
+      setLoading(false)
+      setData(response.data)
+    } catch (error: any) {
+      setLoading(false)
+      setError(error.response.data.errorMessage)
+    }
+  }
+
+  return { loading, data, error, fetchAvailability }
+}
+
+export default useAvailability
