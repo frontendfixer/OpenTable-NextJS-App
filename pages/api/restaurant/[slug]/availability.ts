b4c16a1b53b6a5b90618dcbe8aa84793d68349c7
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-24 10:01:59 +0530

    create searchTimeWithTable object

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -73,7 +73,21 @@ export default async function handler(
 
   const tables = restaurant.tables
 
-  return res.json({ searchTimes, bookings, bookingTableObj, tables })
+  const searchTimesWithTables = searchTimes.map((searchTime) => {
+    return {
+      date: new Date(`${day}T${searchTime}`),
+      time: searchTime,
+      tables,
+    }
+  })
+
+  return res.json({
+    searchTimes,
+    bookings,
+    bookingTableObj,
+    tables,
+    searchTimesWithTables,
+  })
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-08-24&time=14:00:00.000Z&partySize=4
