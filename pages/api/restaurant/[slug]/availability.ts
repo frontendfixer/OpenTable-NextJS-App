99927d891da1b67f8325bcaab472827fd38b5419
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-25 09:02:54 +0530

    only accept GET request

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -9,71 +9,76 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { slug, day, time, partySize } = req.query as {
-    slug: string
-    day: string
-    time: string
-    partySize: string
-  }
+  if (req.method === 'GET') {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string
+      day: string
+      time: string
+      partySize: string
+    }
 
-  if (!day || !time || !partySize) {
-    return res.status(400).json({
-      errorMessage: 'Invalid data provided in url',
+    if (!day || !time || !partySize) {
+      return res.status(400).json({
+        errorMessage: 'Invalid data provided in url',
+      })
+    }
+
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug: slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+      },
     })
-  }
+    if (!restaurant) {
+      return res.status(400).json({
+        errorMessage: 'Invalid data provided in time',
+      })
+    }
 
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug: slug,
-    },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-    },
+    const searchTimesWithTables = await findAvailableTable({
+      time,
+      day,
+      res,
+      restaurant,
+    })
+
+    if (!searchTimesWithTables) {
+      return res.status(400).json({
+        errorMessage: 'Invalid data provided in url',
+      })
+    }
+
+    const availabilities = searchTimesWithTables
+      .map((t) => {
+        const sumOfSeats = t.tables.reduce((sum, table) => {
+          return sum + table.seats
+        }, 0)
+
+        return {
+          time: t.time,
+          seats: sumOfSeats,
+          available: sumOfSeats >= parseInt(partySize),
+        }
+      })
+      .filter((availability) => {
+        const timeIsAfterOpeningHour =
+          new Date(`${day}T${availability.time}`) >=
+          new Date(`${day}T${restaurant.open_time}`)
+        const timeIsBeforeClosingHour =
+          new Date(`${day}T${availability.time}`) <=
+          new Date(`${day}T${restaurant.close_time}`)
+        return timeIsAfterOpeningHour && timeIsBeforeClosingHour
+      })
+
+    return res.json(availabilities)
+  }
+  return res.status(401).json({
+    errorMessage: 'Invalid request',
   })
-  if (!restaurant) {
-    return res.status(400).json({
-      errorMessage: 'Invalid data provided in time',
-    })
-  }
-
-  const searchTimesWithTables = await findAvailableTable({
-    time,
-    day,
-    res,
-    restaurant,
-  })
-
-  if (!searchTimesWithTables) {
-    return res.status(400).json({
-      errorMessage: 'Invalid data provided in url',
-    })
-  }
-
-  const availabilities = searchTimesWithTables
-    .map((t) => {
-      const sumOfSeats = t.tables.reduce((sum, table) => {
-        return sum + table.seats
-      }, 0)
-
-      return {
-        time: t.time,
-        seats: sumOfSeats,
-        available: sumOfSeats >= parseInt(partySize),
-      }
-    })
-    .filter((availability) => {
-      const timeIsAfterOpeningHour =
-        new Date(`${day}T${availability.time}`) >=
-        new Date(`${day}T${restaurant.open_time}`)
-      const timeIsBeforeClosingHour =
-        new Date(`${day}T${availability.time}`) <=
-        new Date(`${day}T${restaurant.close_time}`)
-      return timeIsAfterOpeningHour && timeIsBeforeClosingHour
-    })
-
-  return res.json(availabilities)
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-08-24&time=14:00:00.000Z&partySize=4
