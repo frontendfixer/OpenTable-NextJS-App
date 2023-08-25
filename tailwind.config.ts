828c9afdf779d3f8b8b074a227ad883306877566
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-25 11:55:50 +0530

    speedup pulse animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontSize: {
-        "2xsm": "10px",
-        xsm: "12px",
-        sm: "13px",
-        reg: "15px",
-        lg: "18px",
-        "2xl": "22px",
-        "3xl": "25px",
-        "4xl": "32px",
-        "5xl": "40px",
-        "6xl": "50px",
-        "7xl": "70px",
+        '2xsm': '10px',
+        xsm: '12px',
+        sm: '13px',
+        reg: '15px',
+        lg: '18px',
+        '2xl': '22px',
+        '3xl': '25px',
+        '4xl': '32px',
+        '5xl': '40px',
+        '6xl': '50px',
+        '7xl': '70px',
+      },
+      animation: {
+        pulse: 'pulse 350ms linear infinite',
       },
     },
+    plugins: [],
   },
-  plugins: [],
-};
-export default config;
+}
+export default config
