6255444603a84a0d87c40e4e77f8aacb9866fd70
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-08-25 11:18:15 +0530

    fetch restaurant by slug and render in Header comp

--- app/reserve/[slug]/_components/Header.tsx
+++ app/reserve/[slug]/_components/Header.tsx
@@ -1,15 +1,23 @@
-const Header = () => {
+import Image from 'next/image'
+
+const Header = ({
+  restaurant,
+}: {
+  restaurant: { name: string; main_image: string }
+}) => {
   return (
     <div>
       <h3 className="font-bold">You&apos;re almost done!</h3>
       <div className="mt-5 flex">
-        <img
-          src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-          alt=""
-          className="h-18 w-32 rounded"
+        <Image
+          src={restaurant.main_image}
+          alt={restaurant.name}
+          width={128}
+          height={128}
+          className="rounded"
         />
         <div className="ml-4">
-          <h1 className="text-3xl font-bold">Aiāna Restaurant Collective</h1>
+          <h1 className="text-3xl font-bold">{restaurant.name}</h1>
           <div className="mt-3 flex">
             <p className="mr-6">Tues, 22, 2023</p>
             <p className="mr-6">7:30 PM</p>
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,17 +1,39 @@
-import { Metadata } from 'next'
 import Header from './_components/Header'
 import ReserveForm from './_components/ReserveForm'
+import { PrismaClient } from '@prisma/client'
+import { notFound } from 'next/navigation'
 
-export const metadata: Metadata = {
-  title: 'Reserve table at Milesstone Grill | Open Table Reservation',
-  description: 'Reserve a table at Milesstone Grill Restaurant',
+const prisma = new PrismaClient()
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      name: true,
+      main_image: true,
+    },
+  })
+
+  if (!restaurant) {
+    return notFound()
+  }
+  return restaurant
 }
 
-const Reserve = () => {
+const Reserve = async ({
+  params,
+}: {
+  params: {
+    slug: string
+  }
+}) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug)
   return (
     <div className="h-screen border-t">
       <div className="m-auto w-3/5 py-9">
-        <Header />
+        <Header restaurant={restaurant} />
         <ReserveForm />
       </div>
     </div>
